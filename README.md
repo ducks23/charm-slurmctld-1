9ccfc286a95f36a2b69616ca8517a85902c2d332
jesse <jesse1517@gmail.com>
2020-07-15 00:30:04 +0000

    updated readme

--- README.md
+++ README.md
@@ -4,15 +4,8 @@
 
 <p align="center"><b>This is The Slurmctld charm for The Slurm Workload Manager</b>, <i>"The Slurm Workload Manager (formerly known as Simple Linux Utility for Resource Management or SLURM), or Slurm, is a free and open-source job scheduler for Linux and Unix-like kernels, used by many of the world's supercomputers and computer clusters."</i></p>
 
-Requirements
-------------
-- Ubuntu 20.04
-- juju 2.8.1
-- charmcraft
 
-
-Quickstart
-----------
+## Quickstart
 ```bash
 juju deploy slurmctld
 ```
@@ -23,11 +16,9 @@ git clone git@github.com:omnivector-solutions/charm-slurmctld && cd charm-slurmc
 charmcraft build
 juju deploy ./slurmctld.charm --resource slurm=/path/to/slurm.tar.gz
 ```
-Interfaces
-----------
+## Interfaces
 - https://github.com/omnivector-solutions/interface-slurmd
 - https://github.com/omnivector-solutions/ops-interface-slurmdbd
 
-Components
-----------
+## Components
 - https://github.com/omnivector-solutions/slurm-ops-manager
--- src/charm.py
+++ src/charm.py
@@ -17,33 +17,45 @@ logger = logging.getLogger()
 
 
 class SlurmctldCharm(CharmBase):
-    """Operator charm responsible for coordinating lifecycle operations for slurmctld."""
+    """Operator charm responsible for lifecycle operations for slurmctld."""
 
     def __init__(self, *args):
         """Initialize charm and configure states and events to observe."""
         super().__init__(*args)
-
         self.slurm_ops_manager = SlurmOpsManager(self, "slurmctld")
         self.slurmdbd = SlurmdbdRequiresRelation(self, "slurmdbd")
         self.slurmd = SlurmdRequires(self, "slurmd")
+        event_handler_bindings = {
+            self.on.install:
+            self._on_install,
 
-        self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.start, self._on_check_status_and_write_config)
+            self.on.start:
+            self._on_check_status_and_write_config,
 
-        self.framework.observe(self.slurmdbd.on.slurmdbd_available, self._on_check_status_and_write_config)
-        self.framework.observe(self.slurmd.on.slurmd_available, self._on_check_status_and_write_config)
-        self.framework.observe(self.slurmd.on.slurmd_unavailable, self._on_check_status_and_write_config)
+            self.slurmdbd.on.slurmdbd_available:
+            self._on_check_status_and_write_config,
+
+            self.slurmd.on.slurmd_available:
+            self._on_check_status_and_write_config,
+
+            self.slurmd.on.slurmd_unavailable:
+            self._on_check_status_and_write_config,
+        }
+        for event, handler in event_handler_bindings.items():
+            self.framework.observe(event, handler)
 
     def _on_install(self, event):
         self.slurm_ops_manager.prepare_system_for_slurm()
         self.unit.status = ActiveStatus("Slurm Installed")
 
     def _on_check_status_and_write_config(self, event):
-        if not (self.slurmdbd.slurmdbd_acquired and self.slurmd.slurmd_acquired):
-            if not self.slurmdbd.slurmdbd_acquired:
-                self.unit.status = BlockedStatus("Slurm NOT AVAILABLE - NEED RELATION TO SLURMDBD")
+        slurmdbd_acquired = self.slurmdbd.slurmdbd_acquired
+        slurmd_acquired = self.slurmd.slurmd_acquired
+        if not (slurmdbd_acquired and slurmd_acquired):
+            if not slurmdbd_acquired:
+                self.unit.status = BlockedStatus("NEED RELATION TO SLURMDBD")
             else:
-                self.unit.status = BlockedStatus("Slurm NOT AVAILABLE - NEED RELATION TO SLURMD")
+                self.unit.status = BlockedStatus("NEED RELATION TO SLURMD")
             event.defer()
         else:
             try:
